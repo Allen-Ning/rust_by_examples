f3f795b4539d4b29fb7a84fd073e4da5b3f2e07a
Allen Ning <npc1222@icloud.com>
2020-06-27 00:10:59 +1000

    added variable bindings

---
+++ src/variable_bindings/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let one = 1i32;
+    let two = 2i64;
+    let three = 3;
+    let four = three;
+    let five = (1, 2);
+    println!("{:?}", one);
+    println!("{:?}", two);
+    println!("{:?}", three);
+    println!("{:?}", four);
+    println!("{:?}", five);
+}
