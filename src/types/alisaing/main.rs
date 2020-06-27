72e7592a3417ee652029ebe3dd28ca3814c29ee0
Allen Ning <npc1222@icloud.com>
2020-06-27 16:07:09 +1000

    added alisaing

---
+++ src/types/alisaing/main.rs
@@ -0,0 +1,11 @@
+type num1 = i32;
+type num2 = f32;
+type num3 = i32;
+type num5 = i128;
+
+fn main() {
+    let one : num3 = 10 as num1;
+    let two = 10 as num5;
+    println!("one: {}", one);
+    println!("two: {}", two);
+}
